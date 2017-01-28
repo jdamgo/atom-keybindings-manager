8e7e48483e1000932060e369c6a4524667566f16
James Damian Gordon <jdamgo@users.noreply.github.com>
2017-01-28 17:25:26 +0100

    initial commit (rev1)

---
+++ ~spec/keybindings-manager-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeybindingsManagerView from '../lib/keybindings-manager-view';
+
+describe('KeybindingsManagerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
