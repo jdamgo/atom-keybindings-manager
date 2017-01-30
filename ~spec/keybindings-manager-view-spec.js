dd14fd1d9421826e11b91330788f271255775f2d
James Damian Gordon <jdamgo@users.noreply.github.com>
2017-01-30 07:57:41 +0100

    desired binding remove file

--- ~spec/keybindings-manager-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import KeybindingsManagerView from '../lib/keybindings-manager-view';
-
-describe('KeybindingsManagerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
